85dfb9daa2fcbd150b5b03c1d99baf033400b934
Pedro H B Cavalcante <pedrohbcavalcante@outlook.com>
2016-12-08 00:59:34 -0300

    nao lembro

--- js/javascript.js
+++ js/javascript.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $(".scroll").click(function(event){
         event.preventDefault();
         $("html, body").animate({
-            scrollTop: $(this.hash).offset().top - (800)
+            scrollTop: $(this.hash).offset().top
         }, 500);
     });
 });
