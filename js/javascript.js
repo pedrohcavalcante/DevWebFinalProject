5c46d9e4079ae698332fc83d4876783f04ed3a8f
Pedro H B Cavalcante <pedrohbcavalcante@outlook.com>
2016-12-08 00:53:44 -0300

    Jquery

--- js/javascript.js
+++ js/javascript.js
@@ -1,10 +1,8 @@
 $(document).ready(function() {
-
-    // Handles scrolling animation
     $(".scroll").click(function(event){
         event.preventDefault();
         $("html, body").animate({
-            scrollTop: $(this.hash).offset().top - (3 * 16) // 3 * 16 = 3em
+            scrollTop: $(this.hash).offset().top - (800)
         }, 500);
-    })
+    });
 });
